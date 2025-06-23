9d7346155f0fee4269136952d2ae566d938159ef
nora968-lkj <nora6185g@gmail.com>
2025-06-24 01:35:06 +0300

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -21,10 +21,10 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+    for book in self.books:
+        if isinstance(book, EBook):
+            print("EBook: {} by {}, File Size: {}KB".format(book.title, book.author, book.file_size))
+        elif isinstance(book, PrintBook):
+            print("PrintBook: {} by {}, Page Count: {}".format(book.title, book.author, book.page_count))
+        else:
+            print("Book: {} by {}".format(book.title, book.author))
