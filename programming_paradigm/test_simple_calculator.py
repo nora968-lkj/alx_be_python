1cadda924cc03ec4c4279773e8f050ecc8b44acc
nora968-lkj <nora6185g@gmail.com>
2025-07-03 23:34:51 +0300

    programming_paradigm

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -7), -12)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-5, -3), -2)
+        self.assertEqual(self.calc.subtract(10, 15), -5)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertIsNone(self.calc.divide(5, 0))
+
+if __name__ == "__main__":
+    unittest.main()
