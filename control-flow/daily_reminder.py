8c6497367b41be603e084f4c6ba26b565e88f47c
nora968-lkj <nora6185g@gmail.com>
2025-07-04 01:01:32 +0300

    control-flow

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,11 +6,11 @@ match priority:
     case "high":
         reminder = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        reminder = f"Note: '{task}' is a medium priority task"
+        reminder = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        reminder = f"Note: '{task}' is a low priority task"
+        reminder = f"Reminder: '{task}' is a low priority task"
     case _:
-        reminder = f"'{task}' has an unknown priority"
+        reminder = f"Reminder: '{task}' has an unknown priority"
 
 if time_bound == "yes":
     reminder += " that requires immediate attention today!"
